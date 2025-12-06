40e44d89caec2009e80ed6f1e55ed9b7e21900f6
hari-3061 <hariharanshariharan986@gmail.com>
2025-12-06 21:34:23 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,63 +1,28 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import Landing from "./pages/Landing";
+import UserRegister from "./pages/UserRegister";
+import UserLogin from "./pages/UserLogin";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import UserDashboard from "./pages/UserDashboard";
 
 function App() {
-  const [page, setPage] = useState("register"); // register | login | forgot
-
   return (
-    <div className="main">
-      {/* REGISTER PAGE */}
-      {page === "register" && (
-        <div className="form-container">
-          <h2>Register Now</h2>
-          <input type="text" placeholder="Enter Name" />
-          <input type="email" placeholder="Enter Email" />
-          <input type="password" placeholder="Create Password" />
-          <input type="text" placeholder="Phone Number" />
-
-          <button className="btn">Register</button>
-
-          <p className="link" onClick={() => setPage("login")}>
-            Already have an account? Login
-          </p>
-        </div>
-      )}
-
-      {/* LOGIN PAGE */}
-      {page === "login" && (
-        <div className="form-container">
-          <h2>Login</h2>
-
-          <input type="password" placeholder="Enter Password" />
-
-          <button className="btn">Login</button>
-
-          <p className="forgot" onClick={() => setPage("forgot")}>
-            Forgot Password?
-          </p>
-
-          <p className="link" onClick={() => setPage("register")}>
-            Create New Account
-          </p>
-        </div>
-      )}
-
-      {/* FORGOT PASSWORD PAGE */}
-      {page === "forgot" && (
-        <div className="form-container">
-          <h2>Reset Password</h2>
-
-          <input type="email" placeholder="Enter Your Email" />
-
-          <button className="btn">Send Reset Link</button>
-
-          <p className="link" onClick={() => setPage("login")}>
-            Back to Login
-          </p>
-        </div>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/userregister" element={<UserRegister />} />
+        <Route path="/userlogin" element={<UserLogin />} />
+        <Route path="/adminlogin" element={<AdminLogin />} />
+        <Route path="/admindashboard" element={<AdminDashboard />} />
+        <Route path="/userdashboard" element={<UserDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
